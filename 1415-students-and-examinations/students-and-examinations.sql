d49162976e2f80c230f88c26d1235bfd91164d88
William Zhang <98196084+Ext1nct9@users.noreply.github.com>
2024-04-10 19:03:16 -0400

    Time: 1918 ms (40.09%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -3,5 +3,5 @@ SELECT Students.student_id, Students.student_name, Subjects.subject_name, COUNT(
 FROM Students
 CROSS JOIN Subjects
 LEFT JOIN Examinations ON (Examinations.subject_name = Subjects.subject_name AND Students.student_id = Examinations.student_id)
-GROUP BY Students.student_id, Subjects.subject_name
-ORDER BY Students.student_id
+GROUP BY Students.student_id, Students.student_name, Subjects.subject_name
+ORDER BY Students.student_id, Subjects.subject_name
